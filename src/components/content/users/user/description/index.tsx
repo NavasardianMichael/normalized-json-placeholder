adcda248d0c89110baf806b01ef1c77ca00396ba
NavasardianMichael <navasardianmichael@gmail.com>
2025-04-01 18:03:09 +0400

    test

--- src/components/content/users/user/description/index.tsx
+++ src/components/content/users/user/description/index.tsx
@@ -1,42 +1,36 @@
 import { FC } from 'react'
-import { Divider, Flex } from 'antd'
 import { selectAlbums } from 'store/albums/selectors'
 import { selectPosts } from 'store/posts/selectors'
 import { selectTodos } from 'store/todos/selectors'
-import { User } from 'store/users/types'
+import { User as UserType } from 'store/users/types'
 import { useAppSelector } from 'hooks/useAppSelector'
-import { DescriptionDetail } from './Detail'
 
 type Props = {
-  data: User
+  data: UserType
 }
 
-const userDetails: (keyof User)[] = ['name', 'email', 'address', 'phone', 'companyName']
+const DETAIL_KEYS: (keyof UserType)[] = ['name', 'email', 'address', 'phone', 'companyName']
 
-export const Description: FC<Props> = ({ data }) => {
+export const User: FC<Props> = ({ data }) => {
   const albums = useAppSelector(selectAlbums)
   const todos = useAppSelector(selectTodos)
   const posts = useAppSelector(selectPosts)
 
   return (
-    <Flex gap="small" vertical style={{ marginTop: 12, whiteSpace: 'nowrap' }}>
-      {userDetails.map((detailKey) => {
-        return <DescriptionDetail key={detailKey} name={detailKey} value={data[detailKey]} />
+    <>
+      {/* Here are listed the details, which directly characterize the user */}
+      {DETAIL_KEYS.map((key) => {
+        return (
+          <p key={key}>
+            {key}: {data[key]}
+          </p>
+        )
       })}
 
-      <Divider>Related Entities</Divider>
-      <DescriptionDetail
-        name="Albums"
-        value={albums.idsByUserId[data.id].map((albumId) => albums.byId[albumId].title).join(', ')}
-      />
-      <DescriptionDetail
-        name="Todos"
-        value={todos.idsByUserId[data.id].map((todoId) => todos.byId[todoId].title).join(', ')}
-      />
-      <DescriptionDetail
-        name="Posts"
-        value={posts.idsByUserId[data.id].map((postId) => posts.byId[postId].title).join(', ')}
-      />
-    </Flex>
+      {/* And here are listed the details, retrieved from related entities */}
+      <p>Albums: {albums.idsByUserId[data.id].map((albumId) => albums.byId[albumId].title).join(', ')}</p>
+      <p>Todos: {todos.idsByUserId[data.id].map((todoId) => todos.byId[todoId].title).join(', ')}</p>
+      <p>Posts: {posts.idsByUserId[data.id].map((postId) => posts.byId[postId].title).join(', ')}</p>
+    </>
   )
 }
